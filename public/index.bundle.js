f2a2d66aa46e09f505d1cc960c23d76db67b533d
NicholasGalarza <nicogala93@gmail.com>
2017-12-29 13:08:19 -0500

    housecleaning

--- public/index.bundle.js
+++ public/index.bundle.js
@@ -1,31 +1,31 @@
 webpackJsonp([1],{
 
-/***/ 132:
+/***/ 86:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 /* All aframe related dependencies & aframe components */
 
-__webpack_require__(133);
+__webpack_require__(87);
 
-__webpack_require__(135);
+__webpack_require__(89);
 
-__webpack_require__(136);
+__webpack_require__(90);
 
-__webpack_require__(137);
+__webpack_require__(91);
 
-__webpack_require__(138);
+__webpack_require__(92);
 
-__webpack_require__(139);
+__webpack_require__(93);
 
-__webpack_require__(140);
+__webpack_require__(94);
 
-__webpack_require__(141);
+__webpack_require__(95);
 
 /***/ }),
 
-/***/ 133:
+/***/ 87:
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(global, setImmediate) {var require;var require;(function(f){if(true){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AFRAME = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return require(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
@@ -79488,11 +79488,11 @@ module.exports = getWakeLock();
 });
 //# sourceMappingURL=aframe-master.js.map
 
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(40), __webpack_require__(79).setImmediate))
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(25), __webpack_require__(52).setImmediate))
 
 /***/ }),
 
-/***/ 135:
+/***/ 89:
 /***/ (function(module, exports) {
 
 if (typeof AFRAME === 'undefined') {
@@ -79612,7 +79612,7 @@ AFRAME.registerComponent('random-scale', {
 
 /***/ }),
 
-/***/ 136:
+/***/ 90:
 /***/ (function(module, exports) {
 
 if (typeof AFRAME === 'undefined') {
@@ -79644,7 +79644,7 @@ AFRAME.registerComponent('entity-generator', {
 
 /***/ }),
 
-/***/ 137:
+/***/ 91:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -79669,7 +79669,7 @@ AFRAME.registerComponent('face-watcher', {
 
 /***/ }),
 
-/***/ 138:
+/***/ 92:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -79712,7 +79712,7 @@ AFRAME.registerComponent('boundary', {
 
 /***/ }),
 
-/***/ 139:
+/***/ 93:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -79754,7 +79754,7 @@ AFRAME.registerComponent('generate-asteroids', {
 
 /***/ }),
 
-/***/ 140:
+/***/ 94:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -79825,7 +79825,7 @@ AFRAME.registerComponent("projectile", {
 
 /***/ }),
 
-/***/ 141:
+/***/ 95:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -79883,5 +79883,5 @@ AFRAME.registerComponent("spawner", {
 
 /***/ })
 
-},[132]);
+},[86]);
 //# sourceMappingURL=index.bundle.js.map
