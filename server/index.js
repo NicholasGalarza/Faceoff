50c16391bfb5094d39f83d3877f0e4621c5efc65
NicholasGalarza <nicogala93@gmail.com>
2017-12-29 19:00:47 -0500

    refactor: before deploy

--- server/index.js
+++ server/index.js
@@ -1,21 +1,16 @@
 'use strict'
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080
 
-const express = require('express');
-const path = require('path');
+const app = express()
 
-const app = express();
-
-const isProduction = process.env.NODE_ENV === 'production';
-const port = isProduction ? process.env.PORT : 8080;
-
-var publicPath = path.resolve(__dirname, '../public');
-
-app.use(express.static(publicPath));
+app.use(express.static(path.resolve(__dirname, '../public')))
 
 app.use('/', (req, res, send) => {
   res.sendFile(path.resolve(__dirname, '..', 'index.html'))
 })
 
 app.listen(port, () => {
- console.log(`Virtual Reality Enabled On Port ${port}`);
+  console.log(`Virtual Reality Enabled On Port ${port}`)
 });
