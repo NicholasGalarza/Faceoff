91597a2271920ad17bfdfe22b4c2775540c8e5b3
NicholasGalarza <nicogala93@gmail.com>
2017-12-30 14:47:02 -0500

    debugging

--- server/index.js
+++ server/index.js
@@ -1,20 +1,19 @@
 'use strict'
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const production = process.env.NODE_ENV === 'production';
+const production = process.env.NODE_ENV === 'production'
 const port = production ? process.env.PORT : 8080;
-console.log('my port', port)
-console.log('my node env', process.env.NODE_ENV)
-var publicPath = path.resolve(__dirname, '../public');
 
-app.use(express.static(publicPath));
+let publicPath = path.resolve(__dirname, '..', 'public')
+
+app.use(express.static(publicPath))
 
 app.use('/', (req, res, send) => {
   res.sendFile(path.resolve(__dirname, '..', 'index.html'))
 })
 
 app.listen(port, () => {
- console.log(`Virtual Reality Enabled On Port ${port}`);
+ console.log(`Virtual Reality Enabled On Port ${port}`)
 });
